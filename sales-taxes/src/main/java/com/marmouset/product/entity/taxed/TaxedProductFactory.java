8cdf658af80fa44affebd54d0c6ce40629798bec
Simon Serrano <simon.serrano@busit.com>
2025-04-07 14:35:17 +0200

    refactor: added taxed product factory

---
+++ sales-taxes/src/main/java/com/marmouset/product/entity/taxed/TaxedProductFactory.java
@@ -0,0 +1,19 @@
+package com.marmouset.product.entity.taxed;
+
+import com.marmouset.price.entity.Price;
+import com.marmouset.product.entity.Product;
+
+/**
+ * This interface describes how to create a taxed product.
+ * Classes implementing this class are responsible for TaxedProduct creation
+ */
+public interface TaxedProductFactory {
+  /**
+   * Creates a taxed product based on a product and a taxed price.
+   *
+   * @param product    the actual product
+   * @param taxedPrice the taxed price
+   * @return the taxed product
+   */
+  TaxedProduct create(Product product, Price taxedPrice);
+}
