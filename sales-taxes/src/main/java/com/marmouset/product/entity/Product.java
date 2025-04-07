fa9cb94186a2cc90251a64971ae2237a2615868d
Simon Serrano <simon.serrano@busit.com>
2025-04-07 14:32:47 +0200

    docs: added product java doc

--- sales-taxes/src/main/java/com/marmouset/product/entity/Product.java
+++ sales-taxes/src/main/java/com/marmouset/product/entity/Product.java
@@ -1,9 +1,29 @@
 package com.marmouset.product.entity;
 
+import com.marmouset.price.entity.Price;
+
+/**
+ * This interface represents a product.
+ */
 public interface Product {
+  /**
+   * Whether the product is imported.
+   *
+   * @return true if the product is imported
+   */
   boolean isImported();
 
+  /**
+   * Get the products category.
+   *
+   * @return the category
+   */
   Category getCategory();
 
-  float getPrice();
+  /**
+   * Get the product's price.
+   *
+   * @return the price
+   */
+  Price getPrice();
 }
