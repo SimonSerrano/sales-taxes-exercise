31dab3544bbfab0c0be843d74986b004700d0890
Simon Serrano <simon.serrano@busit.com>
2025-04-07 14:35:53 +0200

    refactor: added receipt factory java doc and updated creation method

--- sales-taxes/src/main/java/com/marmouset/receipt/entity/ReceiptFactory.java
+++ sales-taxes/src/main/java/com/marmouset/receipt/entity/ReceiptFactory.java
@@ -1,5 +1,19 @@
 package com.marmouset.receipt.entity;
 
+import java.util.Collection;
+
+import com.marmouset.product.entity.taxed.TaxedProduct;
+
+/**
+ * This interface describes how to create receipts.
+ * Classes implementing this interface are responsible for creatin Receipts
+ */
 public interface ReceiptFactory {
-  Receipt create();
+  /**
+   * Creates a receipt from products.
+   *
+   * @param products the products
+   * @return a receipt
+   */
+  Receipt create(Collection<TaxedProduct> products);
 }
