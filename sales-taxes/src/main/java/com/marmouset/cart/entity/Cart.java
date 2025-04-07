cf687c98e169435eed8049b5f6adfbe831b0f293
Simon Serrano <simon.serrano@busit.com>
2025-04-07 14:32:13 +0200

    docs: added cart java doc

--- sales-taxes/src/main/java/com/marmouset/cart/entity/Cart.java
+++ sales-taxes/src/main/java/com/marmouset/cart/entity/Cart.java
@@ -1,11 +1,24 @@
 package com.marmouset.cart.entity;
 
+import com.marmouset.product.entity.Product;
 import java.util.Collection;
 
-import com.marmouset.product.entity.Product;
-
+/**
+ * This interface represents a Cart.
+ */
 public interface Cart {
+  /**
+   * Adds a product to the cart.
+   *
+   * @param product the product
+   * @return this
+   */
   Cart add(Product product);
 
+  /**
+   * Gets the products in the cart.
+   *
+   * @return the products
+   */
   Collection<? extends Product> getProducts();
 }
