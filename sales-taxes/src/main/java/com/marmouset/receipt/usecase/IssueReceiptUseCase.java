f6f0c0a5f3330dc3bf580724794614f1825f2d4e
Simon Serrano <simon.serrano@busit.com>
2025-04-07 14:36:17 +0200

    docs: added java doc

--- sales-taxes/src/main/java/com/marmouset/receipt/usecase/IssueReceiptUseCase.java
+++ sales-taxes/src/main/java/com/marmouset/receipt/usecase/IssueReceiptUseCase.java
@@ -3,7 +3,16 @@ package com.marmouset.receipt.usecase;
 import com.marmouset.cart.entity.Cart;
 import com.marmouset.receipt.entity.Receipt;
 
+/**
+ * This class describes how to issue a receipt.
+ */
 public interface IssueReceiptUseCase {
 
+  /**
+   * Issue a receipt from a cart.
+   *
+   * @param cart the cart
+   * @return the receipt
+   */
   Receipt issueReceipt(Cart cart);
 }
